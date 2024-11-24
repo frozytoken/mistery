68c28b625f7bfd696617e41fb7af02de58c3e75c
frozytoken <frozytoken@gmail.com>
2024-11-25 00:32:02 +0100

    Generador

--- src/components/Avatares/AvatarGenerator.js
+++ src/components/Avatares/AvatarGenerator.js
@@ -3,13 +3,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDice, faSync, faDownload } from "@fortawesome/free-solid-svg-icons";
 import "./AvatarGeneratorDesktop.css";
 import backImage from "../../assets/backpfp.png";
+import html2canvas from "html2canvas";
 
 const categories = {
   background: ["blue.png", "beach.jpg", "blue2.png", "blueyellow.png", "green.png"],
   tshirt: ["bonesbody.png", "flankensteinbody.png", "mummybody.png", "pumpkinbody.png"],
   mouth: ["2teethmouth.png", "happymouth.png", "lipmouth.png", "meowmouth.png"],
   hair: ["afrohair.png", "bluehair.png", "relaxhair.png"],
-  headcomplements: ["afrohead.png", "brainhead.png", "explosionhead.png"],
+  complements: ["afrohead.png", "brainhead.png", "explosionhead.png"],
   eyes: ["eye1.png", "eye2.png", "eye3.png", "eye4.png"],
   accesories: ["aposit.png", "goldearrings.png", "piercingsilverright.png"],
   glasses: ["3dglasses.png", "monoculo.png", "glasses6.png"],
@@ -21,11 +22,12 @@ const AvatarGenerator = () => {
     tshirt: null,
     mouth: null,
     hair: null,
-    headcomplements: null,
+    complements: null,
     eyes: null,
     accesories: null,
     glasses: null,
   });
+  const [customText, setCustomText] = useState("Type Here");
 
   const avatarRef = useRef();
 
@@ -59,49 +61,68 @@ const AvatarGenerator = () => {
       tshirt: null,
       mouth: null,
       hair: null,
-      headcomplements: null,
+      complements: null,
       eyes: null,
       accesories: null,
       glasses: null,
     });
+    setCustomText("$DVIL. #ThePumperMan");
   };
 
-  const handleDownload = () => {
-    const canvas = document.createElement("canvas");
-    canvas.width = 500;
-    canvas.height = 500;
-    const ctx = canvas.getContext("2d");
-
-    const drawLayer = (src) => {
-      return new Promise((resolve) => {
-        const img = new Image();
-        img.crossOrigin = "Anonymous";
-        img.src = src;
-        img.onload = () => {
-          ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-          resolve();
-        };
+  const handleDownload = (includeCustomText) => {
+    if (includeCustomText) {
+      // Descargar todo el contenedor incluyendo marco y texto
+      html2canvas(avatarRef.current, {
+        useCORS: true, // Permite cargar imágenes externas
+        scale: 5, // Incrementa la calidad de la imagen
+        backgroundColor: "#ffffff", // Aseguramos un fondo blanco
+        width: avatarRef.current.offsetWidth, // Asegura el tamaño exacto
+        height: avatarRef.current.offsetHeight, // Asegura el tamaño exacto
+      }).then((canvas) => {
+        const link = document.createElement("a");
+        link.download = "full-avatar.png";
+        link.href = canvas.toDataURL("image/png");
+        link.click();
       });
-    };
-
-    const drawAvatar = async () => {
-      await drawLayer(getImagePath("background", selectedItems.background));
-      await drawLayer(`${process.env.PUBLIC_URL}/assets/avatares/body/bodyblue.png`);
-
-      for (const [category, index] of Object.entries(selectedItems)) {
-        if (index !== null && category !== "background") {
-          await drawLayer(getImagePath(category, index));
+    } else {
+      // Descargar solo la imagen interior del avatar
+      const canvas = document.createElement("canvas");
+      canvas.width = 1000; // Define la resolución deseada
+      canvas.height = 1000;
+      const ctx = canvas.getContext("2d");
+  
+      const drawLayer = (src) => {
+        return new Promise((resolve) => {
+          const img = new Image();
+          img.crossOrigin = "Anonymous"; // Permite cargar imágenes externas
+          img.src = src;
+          img.onload = () => {
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            resolve();
+          };
+        });
+      };
+  
+      const drawAvatar = async () => {
+        await drawLayer(getImagePath("background", selectedItems.background));
+        await drawLayer(`${process.env.PUBLIC_URL}/assets/avatares/body/bodyblue.png`);
+  
+        for (const [category, index] of Object.entries(selectedItems)) {
+          if (index !== null && category !== "background") {
+            await drawLayer(getImagePath(category, index));
+          }
         }
-      }
-
-      const link = document.createElement("a");
-      link.download = "avatar.png";
-      link.href = canvas.toDataURL("image/png");
-      link.click();
-    };
-
-    drawAvatar();
+  
+        const link = document.createElement("a");
+        link.download = "inner-avatar.png";
+        link.href = canvas.toDataURL("image/png");
+        link.click();
+      };
+  
+      drawAvatar();
+    }
   };
+  
 
   return (
     <div
@@ -139,28 +160,38 @@ const AvatarGenerator = () => {
               ) : null
             )}
           </div>
-          <p className="avatar-code">B1-B1-E1</p>
+          {/* Input para el texto personalizado */}
+          <input
+            type="text"
+            value={customText}
+            onChange={(e) => setCustomText(e.target.value)}
+            className="avatar-code-input"
+            placeholder="Enter custom text"
+          />
         </div>
 
         {/* Controls */}
         <div className="generator-controls">
-          {Object.keys(categories).map((category) => (
-            <div key={category} className="control-group">
-              <button
-                className="control-button"
-                onClick={() => handleNavigate(category, "left")}
-              >
-                {"<"}
-              </button>
-              <span className="control-label">{category}</span>
-              <button
-                className="control-button"
-                onClick={() => handleNavigate(category, "right")}
-              >
-                {">"}
-              </button>
-            </div>
-          ))}
+          {/* Sección con overlay */}
+          <div className="control-section">
+            {Object.keys(categories).map((category) => (
+              <div key={category} className="control-group">
+                <button
+                  className="control-button"
+                  onClick={() => handleNavigate(category, "left")}
+                >
+                  {"<"}
+                </button>
+                <span className="control-label">{category}</span>
+                <button
+                  className="control-button"
+                  onClick={() => handleNavigate(category, "right")}
+                >
+                  {">"}
+                </button>
+              </div>
+            ))}
+          </div>
 
           {/* Action Buttons */}
           <div className="action-buttons">
@@ -170,9 +201,20 @@ const AvatarGenerator = () => {
             <button className="reset-button" onClick={handleReset}>
               <FontAwesomeIcon icon={faSync} /> Reset traits
             </button>
-            <button className="download-button" onClick={handleDownload}>
-              <FontAwesomeIcon icon={faDownload} /> Download
-            </button>
+            <div className="download-buttons">
+              <button
+                className="download-button"
+                onClick={() => handleDownload(true)}
+              >
+                <FontAwesomeIcon icon={faDownload} /> Full Avatar
+              </button>
+              <button
+                className="download-button"
+                onClick={() => handleDownload(false)}
+              >
+                <FontAwesomeIcon icon={faDownload} /> Inner Avatar
+              </button>
+            </div>
           </div>
         </div>
       </main>
